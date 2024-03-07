2d14abda174616abfba02713b99b85a035625046
Maor <24mbaker@gannacademy.org>
2024-03-07 08:31:49 -0500

    save.

--- src/App.java
+++ src/App.java
@@ -6,6 +6,12 @@ public class App extends PApplet {
     }
 
     boolean running = true;
+    boolean key_Up = false;
+    boolean key_Down = false;
+    boolean key_Left = false;
+    boolean key_Right = false;
+    int speed = 5;
+
     int rect1X = 0;
     int rect1Y = 350;
     int rect2X = 450;
@@ -30,10 +36,12 @@ public class App extends PApplet {
     public void draw() {
         if (running == true) {
             background(200);
+            movement();
             ellipse(circleX, circleY, 50, 50);
             if (circleX >= 600 || circleX <= 0) {
 
             }
+            fill(210, 180, 140);
             rect(rect1X, rect1Y, 300, 50);
             rect(rect2X, rect2Y, 150, 50);
             rect(rect3X, rect3Y, 150, 50);
@@ -56,7 +64,8 @@ public class App extends PApplet {
             if (rect4Y == 800) {
                 rect4Y = 0;
             }
-            if(collisionDetection()){
+            if (collisionDetection1()
+                    || collisionDetection2() || collisionDetection3() || collisionDetection4()) {
                 gameOver();
             }
         }
@@ -64,23 +73,53 @@ public class App extends PApplet {
 
     public void keyPressed() {
         if (keyCode == RIGHT) {
-            circleX += 10;
+            key_Right = true;
         }
-        if (keyCode == LEFT && circleX <= 600) {
-            circleX -= 10;
+        if (keyCode == LEFT) {
+            key_Left = true;
         }
         if (keyCode == UP) {
-            circleY -= 10;
+            key_Up = true;
         }
         if (keyCode == DOWN) {
-            circleY += 10;
+            key_Down = true;
         }
         if (keyCode == 32) {
             restart();
         }
     }
 
-    public boolean collisionDetection() {
+    public void keyReleased() {
+        if (keyCode == RIGHT) {
+            key_Right = false;
+        }
+        if (keyCode == LEFT) {
+            key_Left = false;
+        }
+        if (keyCode == UP) {
+            key_Up = false;
+        }
+        if (keyCode == DOWN) {
+            key_Down = false;
+        }
+    }
+
+    public void movement() {
+        if (key_Left && circleX > 0) {
+            circleX -= speed;
+        }
+        if (key_Right && circleX < 600) {
+            circleX += speed;
+        }
+        if (key_Up && circleY > 0) {
+            circleY -= speed;
+        }
+        if (key_Down && circleY < 800) {
+            circleY += speed;
+        }
+    }
+
+    public boolean collisionDetection1() {
 
         if (circleX > rect1X + 300) {
             return false;
@@ -93,7 +132,48 @@ public class App extends PApplet {
         } else {
             return true;
         }
+    }
 
+    public boolean collisionDetection2() {
+        if (circleX > rect2X + 150) {
+            return false;
+        } else if (circleX + 40 < rect2X) {
+            return false;
+        } else if (circleY > rect2Y + 50) {
+            return false;
+        } else if (circleY + 45 < rect2Y) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean collisionDetection3() {
+        if (circleX > rect3X + 150) {
+            return false;
+        } else if (circleX + 50 < rect3X) {
+            return false;
+        } else if (circleY > rect3Y + 50) {
+            return false;
+        } else if (circleY + 50 < rect3Y) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public boolean collisionDetection4() {
+        if (circleX > rect4X + 300) {
+            return false;
+        } else if (circleX + 50 < rect4X) {
+            return false;
+        } else if (circleY > rect4Y + 50) {
+            return false;
+        } else if (circleY + 50 < rect3Y) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public void gameOver() {
@@ -111,15 +191,16 @@ public class App extends PApplet {
         circleX = 300;
         circleY = 700;
 
+        fill(139, 0, 0);
         rect(rect1X, rect1Y, 300, 50);
         rect(rect2X, rect2Y, 150, 50);
         rect(rect3X, rect3Y, 150, 50);
         rect(rect4X, rect4Y, 300, 50);
         ellipse(circleX, circleY, 50, 50);
-        fill(255, 192, 192);
 
+        fill(139, 0, 0);
         textSize(60);
-        text("GAME OVER!", 175, 400);
+        text("GAME OVER!", 160, 300);
 
     }
 
